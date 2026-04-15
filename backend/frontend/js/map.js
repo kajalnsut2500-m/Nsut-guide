c6b835975c821c1cc89062ce3095c20c81edebc7
kajalnsut2500-m <kajal.nsut2500@gmail.com>
2026-04-15 11:22:36 +0530

    Refactor map.js to add custom icons and locations

--- backend/frontend/js/map.js
+++ backend/frontend/js/map.js
@@ -1,40 +1,105 @@
 // Center of NSUT (approx)
-var map = L.map('map').setView([28.6097, 77.0375], 16);
+var map = L.map('map', {
+  zoomControl: false   // this disables Leaflet default + − buttons
+}).setView([28.610, 77.037], 16);
+
 
 // Tile Layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map);
 
-// Locations
-var apj = [28.6105, 77.0380];
-var lt = [28.6090, 77.0368];
-var library = [28.6085, 77.0376];
-var admin = [28.6098, 77.0392];
-var hostel = [28.6112, 77.0359];
-var canteen = [28.6089, 77.0387];
+// Custom Icons
+var blueIcon = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
+var redIcon = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+// Locations
+var APJ_Block = [ 28.611961867266903, 77.03728079795839];
+var Block6 = [28.61056789589633, 77.03773140907289];
+var library = [ 28.6101911437017, 77.03898668289186];
+var admin = [28.609795552443043, 77.03703403472902];
+var hostel = [28.612828380686633, 77.03530669212341];
+var canteen = [28.611622794852398, 77.03694283962251];
+var Block5 = [28.60962601287613, 77.03875064849855];
+var Block4 = [28.60940466914083, 77.03767776489259];
 // Markers
-L.marker(apj).addTo(map).bindPopup("APJ Block");
-L.marker(lt).addTo(map).bindPopup("Lecture Theatre");
-L.marker(library).addTo(map).bindPopup("Library");
-L.marker(admin).addTo(map).bindPopup("Admin Block");
-L.marker(hostel).addTo(map).bindPopup("Hostels");
-L.marker(canteen).addTo(map).bindPopup("Canteen");
+var apjMarker = L.marker(APJ_Block, {icon: blueIcon}).addTo(map)
+  .bindTooltip("<span class='tip-icon'>🏛️</span> APJ Block", {className: 'cute-tooltip', direction: 'top', offset: [0, -38]})
+  .bindPopup("<div class='cute-popup'><span class='pop-icon'>🏛️</span><div><strong>APJ Block</strong><p>Academic Block</p></div></div>");
+
+var block6Marker = L.marker(Block6, {icon: blueIcon}).addTo(map)
+  .bindTooltip("<span class='tip-icon'>🏢</span> Block 6", {className: 'cute-tooltip', direction: 'top', offset: [0, -38]})
+  .bindPopup("<div class='cute-popup'><span class='pop-icon'>🏢</span><div><strong>Block 6</strong><p>Academic Block</p></div></div>");
+
+var block5Marker = L.marker(Block5, {icon: blueIcon}).addTo(map)
+  .bindTooltip("<span class='tip-icon'>🏢</span> Block 5", {className: 'cute-tooltip', direction: 'top', offset: [0, -38]})
+  .bindPopup("<div class='cute-popup'><span class='pop-icon'>🏢</span><div><strong>Block 5</strong><p>Academic Block</p></div></div>");
+
+var block4Marker = L.marker(Block4, {icon: blueIcon}).addTo(map)
+  .bindTooltip("<span class='tip-icon'>🏢</span> Block 4", {className: 'cute-tooltip', direction: 'top', offset: [0, -38]})
+  .bindPopup("<div class='cute-popup'><span class='pop-icon'>🏢</span><div><strong>Block 4</strong><p>Academic Block</p></div></div>");
+
+var libraryMarker = L.marker(library, {icon: blueIcon}).addTo(map)
+  .bindTooltip("<span class='tip-icon'>📚</span> Library", {className: 'cute-tooltip', direction: 'top', offset: [0, -38]})
+  .bindPopup("<div class='cute-popup'><span class='pop-icon'>📚</span><div><strong>Library</strong><p>Central Library</p></div></div>");
+
+var adminMarker = L.marker(admin, {icon: blueIcon}).addTo(map)
+  .bindTooltip("<span class='tip-icon'>🏛️</span> Admin Block", {className: 'cute-tooltip', direction: 'top', offset: [0, -38]})
+  .bindPopup("<div class='cute-popup'><span class='pop-icon'>🏛️</span><div><strong>Admin Block</strong><p>Administration</p></div></div>");
+
+var hostelMarker = L.marker(hostel, {icon: blueIcon}).addTo(map)
+  .bindTooltip("<span class='tip-icon'>🛏️</span> Hostels", {className: 'cute-tooltip', direction: 'top', offset: [0, -38]})
+  .bindPopup("<div class='cute-popup'><span class='pop-icon'>🛏️</span><div><strong>Hostels</strong><p>Student Residences</p></div></div>");
+
+var canteenMarker = L.marker(canteen, {icon: blueIcon}).addTo(map)
+  .bindTooltip("<span class='tip-icon'>🍽️</span> Canteen", {className: 'cute-tooltip', direction: 'top', offset: [0, -38]})
+  .bindPopup("<div class='cute-popup'><span class='pop-icon'>🍽️</span><div><strong>Canteen</strong><p>Food & Beverages</p></div></div>");
+
+var allMarkers = [apjMarker, block6Marker, block5Marker, block4Marker, libraryMarker, adminMarker, hostelMarker, canteenMarker];
 
 // Function
-function goToLocation(location){
+function goToLocation(location, marker){
+  allMarkers.forEach(m => m.setIcon(blueIcon));
+  marker.setIcon(redIcon);
   map.setView(location, 18);
+  marker.openPopup();
+}
+
+// Toggle Sidebar
+function toggleSidebar() {
+  document.getElementById('sidebar').classList.toggle('collapsed');
+  document.getElementById('mainContent').classList.toggle('expanded');
 }
 
 // Simple Search
 document.getElementById("searchBox").addEventListener("keyup", function(){
   let value = this.value.toLowerCase();
 
-  if(value.includes("apj")) goToLocation(apj);
-  else if(value.includes("lt")) goToLocation(lt);
-  else if(value.includes("library")) goToLocation(library);
-  else if(value.includes("admin")) goToLocation(admin);
-  else if(value.includes("hostel")) goToLocation(hostel);
-  else if(value.includes("canteen")) goToLocation(canteen);
+  if(value === "") {
+    allMarkers.forEach(m => m.setIcon(blueIcon));
+    map.setView([28.610, 77.037], 16);
+  }
+  else if(value.includes("apj")) goToLocation(APJ_Block, apjMarker);
+  else if(value.includes("block 6")) goToLocation(Block6, block6Marker);
+  else if(value.includes("block 5")) goToLocation(Block5, block5Marker);
+  else if(value.includes("block 4")) goToLocation(Block4, block4Marker);
+  else if(value.includes("library")) goToLocation(library, libraryMarker);
+  else if(value.includes("admin")) goToLocation(admin, adminMarker);
+  else if(value.includes("hostel")) goToLocation(hostel, hostelMarker);
+  else if(value.includes("canteen")) goToLocation(canteen, canteenMarker);
 });
